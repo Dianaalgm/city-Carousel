64df69acb027f81faf9f9141700dbcb83eccae1b
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-05-03 13:56:13 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { photos } from './models/photos';
+import Card from "./components/Card";
+import Button from "./components/Button";
+import { symbols } from './models/symbols';
+
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [currentImage, setCurrentImage] = useState(photos[count]);
+ //-----(var)-------[functionToUpdate var]------default val for var
+
+  const changePic = () =>{
+    setCount((count ++));
+    console.log("Current Count: ", count);
+    setCurrentImage(photos[count])
+    console.log("Current Image: ", currentImage);
+    //--> [0], [1], [2] = count++
+    
+  };
+//----> Cycle forward through our array of pics
+
+  const reversePic = () => {
+    setCount(count--);
+
+    console.log("Current Count: ", count)
+    setCurrentImage(photos[count])
+    console.log("Current Image: ", currentImage)
+  };
+//----> CYcle Backwards through our array of pics
+
+ //------------------------------------- [State]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> City Directory </h1>
+
+      <Card currentImg={currentImage}/>
+      <div className="btnContainer">
+      <Button action={reversePic}icon={symbols.left}/>
+      <Button action={changePic} icon={symbols.right}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({action,icon}) {
+  return (
+    <button className='btn' onClick={action}>{icon}</button>
+  )
+}
+
+export default Button
